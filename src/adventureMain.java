e5e5dfad73c8c758833842a79bbd8dd54afb8135
unknown <robert.drinnan@gmail.com>
2016-02-21 16:03:59 -0800

    worked on character setup

--- src/adventureMain.java
+++ src/adventureMain.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 
 public class adventureMain {
-    public static void adventure(){
-        System.out.println("test");
+    public static void adventure() {
+        Player player = new Player();
+        setup(player);
+    }
+
+    private static void setup(Player player) {
+        System.out.println("Enter your character name: ");
+        Scanner keyboard = new Scanner(System.in);
+        player.setName(keyboard.next());
+        System.out.println("Do you want to use the deafault stats? y/n");
+        if (keyboard.hasNext("y")) {
+            showPlayerStats(player);
+        } else if (keyboard.hasNext("n")) {
+            keyboard= new Scanner(System.in);
+            System.out.println("Enter " + player.getName() + "'s HP: ");
+            player.setHP(keyboard.nextInt());
+            showPlayerStats(player);
+        }
+
+    }
+
+    public static void showPlayerStats(Player player) {
+        System.out.println("Here are your stats");
+        System.out.println(
+                "Name: " + player.getName() + "\nHP: " + player.getHP() + "\nStrenght: " + player.getStrenght());
     }
 }
--- src/adventureStartScreen.java
+++ src/adventureStartScreen.java
@@ -1,7 +1,8 @@
 
 public class adventureStartScreen {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Awesome Text Adventure\n\nPress any key to Continue");
+        System.out.println("Welcome to the Awesome Text Adventure\n\nPress enter"
+                + " to Continue");
         try {
             System.in.read();
         } catch (Exception e) {
